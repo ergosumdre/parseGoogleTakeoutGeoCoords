2534f170ace9d17391c002d20accbde3327d1e22
ergosumdre <35677602+ergosumdre@users.noreply.github.com>
2020-01-09 15:46:30 -0500

    Create geoData.r
    input needs to be a string of file location

---
+++ geoData.r
@@ -0,0 +1,36 @@
+
+# Parse and returns start and ending lat and long coords
+geoData <- function(x){
+  # JSON to lists
+  locationData <- fromJSON(x)
+  # Parse out Starting Lat coords from lists
+  startLat <- locationData[["timelineObjects"]][["activitySegment"]][["startLocation"]][["latitudeE7"]]
+  # Parse out Starting Long coords from lists
+  startLong <- locationData[["timelineObjects"]][["activitySegment"]][["startLocation"]][["longitudeE7"]]
+  # Parse out Ending Lat coords from lists
+  endLat <- locationData[["timelineObjects"]][["activitySegment"]][["endLocation"]][["latitudeE7"]]
+  # Parse out End Long coords from lists
+  endLong <- locationData[["timelineObjects"]][["activitySegment"]][["endLocation"]][["longitudeE7"]]
+  
+  # Data does not come with decimal points
+  convertToDec <- function(x){
+    # Checks if leading char is a '-' char 
+    if( substr(x, 1,1) == "-" ){
+      # If line has leading '-' char, add a decimal place in 3 location
+      Long <- as.numeric(paste(substr(x, 1,3), ".", substr(x, 4, length(x)), sep = ""))
+    }else{
+      # If line does not have leading '-' char, add a decimal place in the 2 location
+      Lat <- as.numeric(paste(substr(x, 1,2), ".", substr(x, 3, length(x)), sep = ""))
+    }
+  }
+  # Converts to dec
+  startLat2 <- convertToDec(startLat)
+  startLong2 <- convertToDec(startLong)
+  endLat2 <- convertToDec(endLat)
+  endLong2 <- convertToDec(endLong)  
+  
+  # Creats DF with start and ending lat and long coords
+  locationHistory <- data.frame(startLat2, startLong2, endLat2, endLong2)
+  # Removes N/As
+  locationHistory <- remove_missing(locationHistory)
+}
